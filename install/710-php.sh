095689194f20548c82deb3c94ceef9f20553e357
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-01-08 14:29:07 +0800

    Корректировка php

--- install/710-php.sh
+++ install/710-php.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-. "../src/01-common.sh" || exit 5
 PHPVER="8.1"
-title "Installing php${PHPVER}..."
+echo
+echo "==============================================="
+echo "Installing php${PHPVER}..."
+echo "==============================================="
+echo
 
 sudo LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-apti php${PHPVER} \
-     php${PHPVER}-xdebug \
-     php${PHPVER}-bcmath \
-     php${PHPVER}-bz2 \
-     php${PHPVER}-curl \
-     php${PHPVER}-gd \
-     php${PHPVER}-json \
-     php${PHPVER}-mbstring \
-     php${PHPVER}-mysql \
-     php${PHPVER}-opcache \
-     php${PHPVER}-pgsql \
-     php${PHPVER}-soap \
-     php${PHPVER}-xml \
-     php${PHPVER}-xmlrpc \
-     php${PHPVER}-xsl \
-     php${PHPVER}-zip
-   # php${PHPVER}-common
-   # php${PHPVER}-cli
-   # php${PHPVER}-dba
-   # php${PHPVER}-ldap
-   # php${PHPVER}-sqlite3
+sudo apt install -y --autoremove \
+    php${PHPVER} \
+    php${PHPVER}-cli \
+    php${PHPVER}-common \
+    php${PHPVER}-xdebug \
+    # php${PHPVER}-bcmath \
+    # php${PHPVER}-bz2 \
+    # php${PHPVER}-curl \
+    # php${PHPVER}-gd \
+    # php${PHPVER}-json \
+    # php${PHPVER}-mbstring \
+    # php${PHPVER}-mysql \
+    # php${PHPVER}-opcache \
+    # php${PHPVER}-pgsql \
+    # php${PHPVER}-soap \
+    # php${PHPVER}-xml \
+    # php${PHPVER}-xmlrpc \
+    # php${PHPVER}-xsl \
+    # php${PHPVER}-sqlite3 \
+    # php${PHPVER}-zip
+    # php${PHPVER}-dba
+    # php${PHPVER}-ldap
 php -v
