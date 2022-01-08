6d46e36f46c6a99516d0076c9ae707499ebce685
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-01-08 14:18:41 +0800

    Корректировка docker

--- install/020-docker.sh
+++ install/020-docker.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-. "../src/01-common.sh" || exit 5
-title "Installing docker..."
+echo
+echo "==============================================="
+echo "Installing docker..."
+echo "==============================================="
+echo
 
 # https://docs.docker.com/engine/install/ubuntu/
 
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 # sudo apt update
-# apti docker-ce docker-compose
+# sudo apt install -y --autoremove docker-ce docker-compose
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-apti docker-ce docker-ce-cli containerd.io
+sudo apt install -y --autoremove docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker anthony
 
-success "You need to logout and log in again to apply docker group"
+echo "You need to logout and log in again to apply docker group"
