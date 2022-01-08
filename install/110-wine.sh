d7af364574e7f07920d1f2bb7985b48d95afc633
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-01-08 14:24:16 +0800

    Корректировка wine

--- install/110-wine.sh
+++ install/110-wine.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-. "../src/01-common.sh" || exit 5
-title "Installing wine"
+echo
+echo "==============================================="
+echo "Installing wine..."
+echo "==============================================="
+echo
 
 sudo dpkg --add-architecture i386
 wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
-apti winehq-stable
-installed "wine" && success "wine installed!"
+sudo apt install -y --autoremove winehq-stable
+wine --version
