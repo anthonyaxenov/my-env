c4f4452312b338b38dea05bea9af585ae2f26f02
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-01-08 14:34:20 +0800

    Корректировка pgsql

--- install/810-pgsql.sh
+++ install/810-pgsql.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-. "../src/01-common.sh" || exit 5
-title "Installing pgsql..."
+echo
+echo "==============================================="
+echo "Installing postgresql..."
+echo "==============================================="
+echo
 
-apti postgresql postgresql-contrib
+installed() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+sudo apt install -y --autoremove postgresql postgresql-contrib
 sudo service postgresql restart
-installed php && apti php-pgsql
+installed php && sudo apt install -y --autoremove php-pgsql
