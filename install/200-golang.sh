551f80ef29e4925dcd68393b386ec94d906a616b
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-01-09 16:52:56 +0800

    Корректировка golang

--- install/200-golang.sh
+++ install/200-golang.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
-. "../src/01-common.sh" || exit 5
-title "Installing golang"
+echo
+echo "==============================================="
+echo "Installing golang..."
+echo "==============================================="
+echo
 
+# https://go.dev/dl/
 # https://golang.org/doc/install
 # https://www.vultr.com/docs/install-the-latest-version-of-golang-on-ubuntu
 
-# if [ "$EUID" -ne 0 ]
-#   then echo "*** root permissions required ***"
-#   exit
-# fi
-
-[ $1 ] && VERSION="$1" || VERSION="1.17.5"
+[ $1 ] && VERSION="$1" || VERSION="1.17.6"
 FILE="go$VERSION.linux-amd64.tar.gz"
 
 sudo rm -rf /usr/local/go
 wget "https://golang.org/dl/$FILE" -O /tmp/$FILE
 sudo tar -xzf $FILE -C /usr/local
 rm -rf /tmp/$FILE
-sudo chown $USER: -R /usr/local/go
+sudo chown anthony: -R /usr/local/go
 
 echo "export PATH=$PATH:/usr/local/go/bin" >> /home/anthony/.profile
 echo "export GOPATH=~/.go" >> /home/anthony/.profile
 # source ~/.profile
 
-success "$(go version)"
-info "NOTE: now run \`source ~/.profile\` to apply new env vars"
+echo
+go version
+echo "NOTE: now run 'source ~/.profile' to apply new env vars"
+echo
