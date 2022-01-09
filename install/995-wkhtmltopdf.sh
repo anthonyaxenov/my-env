d84e756e076aad4a9ee9cbc563b2914fa34b8e56
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-01-09 16:57:18 +0800

    Корректировка wkhtmltopdf

--- install/995-wkhtmltopdf.sh
+++ install/995-wkhtmltopdf.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-. "../src/01-common.sh" || exit 5
-title "Installing wkhtmltopdf..."
+echo
+echo "==============================================="
+echo "Installing wkhtmltopdf..."
+echo "==============================================="
+echo
 
 # не тестировалось
 
 wget "https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb" -O /tmp/wkhtmltopdf.deb
 sudo dpkg -i /tmp/wkhtmltopdf.deb
-rm -rf /tmp/wkhtmltopdf.deb
+rm /tmp/wkhtmltopdf.deb
