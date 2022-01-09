3f4a7b05042a42134e625a1b74327ad302552ae4
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-01-09 17:07:16 +0800

    Остатки: удалён common.sh и пр мелочи

--- install/120-chrome.sh
+++ install/120-chrome.sh
@@ -7,7 +7,7 @@ echo
 
 # https://t.me/axenov_blog/251
 
-# snapi chromium
+# sudo snap install chromium
 wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O /tmp/google-chrome-stable_current_amd64.deb
 sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb
 rm /tmp/google-chrome-stable_current_amd64.deb
--- start.sh
+++ start.sh
@@ -2,18 +2,10 @@
 set -e
 OLDDIR=`pwd`
 ENVDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ENVSRCDIR="$ENVDIR"/src
 INSTALLDIR="$ENVDIR"/install
 DOTFILESDIR="$ENVDIR"/dotfiles
-
-for script in "$ENVSRCDIR"/*.sh
-do
-    . "$script"
-done
 
 for script in "$INSTALLDIR"/*.sh
 do
     . "$script"
 done
-
-# neofetch
