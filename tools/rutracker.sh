0e90625d7c4f4778961c230557386da6cb236e18
AnthonyAxenov <anthonyaxenov@gmail.com>
2022-10-08 21:50:39 +0800

    Fix for Rutracker's blocked trackers in RF

---
+++ tools/rutracker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "# https://rutracker.net/forum/viewtopic.php?p=75274766#75274766" | sudo tee -a /etc/hosts # > /dev/null
+echo "185.15.211.203 bt.t-ru.org" | sudo tee -a /etc/hosts # > /dev/null
+echo "185.15.211.203 bt2.t-ru.org" | sudo tee -a /etc/hosts # > /dev/null
+echo "185.15.211.203 bt3.t-ru.org" | sudo tee -a /etc/hosts # > /dev/null
+echo "185.15.211.203 bt4.t-ru.org" | sudo tee -a /etc/hosts # > /dev/null
